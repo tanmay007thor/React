45e5e54db9f09c70e90452da039e8a4a87f6fef7
tanmay007thor <76778790+tanmay007thor@users.noreply.github.com>
2022-02-18 21:46:30 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const date = new Date();
+const currentTime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+  color: ""
+};
+
+if (currentTime < 12) {
+  greeting = "code 1";
+  customStyle.color = "red";
+}
